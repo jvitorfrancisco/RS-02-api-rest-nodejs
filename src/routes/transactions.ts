8dcd7f5ca0e48c7f8f015a24b183864c1971837b
João Vitor de Francisco <jvitorfrancisco.dev@gmail.com>
2025-04-22 20:27:44 -0300

    feat: resumo das transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -16,6 +16,12 @@ async function routes (app: FastifyInstance) {
         return { transactions }
     })
 
+    app.get('/summary', async () => {
+        const summary = await knex('transactions').sum('amount', { as: 'amount' }).first()
+
+        return { summary }
+    })
+
     app.get('/:id', async (request) => {
         const getTransactionParamsSchema = z.object({
             id: z.string().uuid(),
