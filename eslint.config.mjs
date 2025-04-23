a07e7e61251f0500a3e4bcd72d49c55de7f05bce
João Vitor de Francisco <jvitorfrancisco.dev@gmail.com>
2025-04-22 22:06:16 -0300

    fix: eslint prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,21 @@
-import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from 'eslint-config-prettier'
+import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    plugins: { prettier: eslintPluginPrettier },
+    rules: {
+      ...eslintConfigPrettier.rules,
+      "prettier/prettier": "error",
+    }
+  }
 ]);
---
+++ src/middlewares/check-session-id-existance.ts
@@ -0,0 +1,10 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export async function checkSessionIdExistance(
+    request: FastifyRequest,
+    reply: FastifyReply
+) {
+    const sessionId = request.cookies.sessionId
+
+    if (!sessionId) reply.status(401).send('Unauthorized')
+}
